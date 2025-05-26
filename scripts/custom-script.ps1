4627b49429a81a6ba07e24f708327b51768b58b8
Chris Durham <chrsdrhm@icloud.com>
2025-05-26 11:08:02 -0500

    Example script to call Microsoft Graph and run via GitHub Actions; masking potentially sensitive items from Actions output

---
+++ scripts/custom-script.ps1
@@ -0,0 +1,32 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$AccessToken, # Access token for Microsoft Graph
+    [switch]$VerboseMode  # Enable verbose output
+)
+
+# Enable verbose output if the switch is provided
+if ($VerboseMode) {
+    $VerbosePreference = "Continue"
+    Write-Verbose "Verbose output enabled"
+}
+
+Write-Output "This is running from the PowerShell script"
+
+# Authenticate to Microsoft Graph using the provided access token
+Connect-MgGraph -AccessToken $(ConvertTo-SecureString -String $AccessToken -AsPlainText -Force) -NoWelcome
+
+# Make a graph call
+$ctx = Get-MgContext
+$maskedDomain = $ctx.Account -replace '@.*', '@****'
+[PSCustomObject]@{
+    ClientId            = "****"
+    TenantId            = "****"
+    Scopes              = "****"
+    AuthType            = $ctx.AuthType
+    TokenCredentialType = $ctx.TokenCredentialType
+    Account             = $maskedDomain
+    AppName             = $ctx.AppName
+    ContextScope        = $ctx.ContextScope
+    PSHostVersion       = $ctx.PSHostVersion
+    Environment         = $ctx.Environment
+} | Format-List
